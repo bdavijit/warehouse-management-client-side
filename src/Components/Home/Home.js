6a9e19718835e74a29a61788510b6c2e453d9964
bdavijit <bdavijit@gmail.com>
2022-04-28 17:10:23 +0600

    personal hook

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useFirebase from '../../Hooks/useFirebase';
 
 const Home = () => {
+
+  const { user } = useFirebase();
     return (
         <div>
-          <h1>This is Home</h1>  
+          <h1>Current User is : {user ? user.displayName : "Vooooot"}</h1>  
         </div>
     );
 };
