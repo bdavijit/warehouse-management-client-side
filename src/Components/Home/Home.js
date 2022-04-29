566c5d343627924225f88a4b4b53183be0f4ce7d
bdavijit <bdavijit@gmail.com>
2022-04-29 21:01:46 +0600

    private route

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import useFirebase from '../../Hooks/useFirebase';
+import Login from '../Login/Login';
 
 const Home = () => {
 
   const { user } = useFirebase();
     return (
         <div>
-          <h1>Current User is : {user ? user.displayName : "Vooooot"}</h1>  
+          {user 
+          ?
+           <h1>Current User is : {user ? user.displayName : "Vooooot"}</h1> 
+           :
+           <Login/>} 
         </div>
     );
 };
--- src/Components/Orders/Order.js
+++ src/Components/Orders/Order.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Order = () => {
     return (
         <div>
-           <h1>This is a order page</h1> 
+          <h1>This is a order page</h1> 
         </div>
     );
 };
