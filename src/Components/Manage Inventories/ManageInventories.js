56acb829b11917c7e769568e71df0345e84ec259
bdavijit <bdavijit@gmail.com>
2022-05-13 12:31:32 +0600

    delete

--- src/Components/Manage Inventories/ManageInventories.js
+++ src/Components/Manage Inventories/ManageInventories.js
@@ -3,39 +3,36 @@ import { useNavigate } from 'react-router-dom';
 import useProducts from '../../Hooks/useProduct';
 import ProductCard2 from '../ProductCard/ProductCard/ProductCard2';
 
-
-
 import './ManageProduct.css';
 
-
 const ManageInventories = () => {
-      
-            const navigate = useNavigate();
-      
-                  const [Products, SetProducts] = useProducts(); 
-                  return (
-                        <>
-                              <button
-                                    className='btn btn-primary'
-                                    onClick={() =>
-                                          navigate('/Add_new')
-                                    }
-                              >
-                                    add new item
-                              </button>
-                              {/* <h1>This is product page</h1> */}
-                              <div className='ProductBox mt-3'>
-                                    {Products?.map((Product) => (
-                                          <ProductCard2
-                                                key={Product.id}
-                                                Product={Product}
-                                          ></ProductCard2>
-                                    ))}
-                              </div>
-                        </>
-                  );
-            
-   
+      const navigate = useNavigate();
+
+      const [Products, SetProducts] = useProducts();
+
+
+
+      return (
+            <>
+                  <button
+                        className='btn btn-primary'
+                        onClick={() => navigate('/Add_new')}
+                  >
+                        add new item
+                  </button>
+                  {/* <h1>This is product page</h1> */}
+                  <div className='ProductBox mt-3'>
+                        {Products?.map((Product) => (
+                              <ProductCard2
+                                    key={Product.id}
+                                    Products={Products}
+                                    Product={Product}
+                                    SetProducts={SetProducts}
+                              ></ProductCard2>
+                        ))}
+                  </div>
+            </>
+      );
 };
 
 export default ManageInventories;
--- src/Components/ProductCard/ProductCard/ProductCard2.js
+++ src/Components/ProductCard/ProductCard/ProductCard2.js
@@ -1,58 +1,79 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./ProductCard2.css"
+import './ProductCard2.css';
 
 const ProductCard2 = (props) => {
-  
-    const {_id, name, image , description, price } = props?.Product;
+      const { _id, name, image, description, price } = props?.Product;
 
       const navigate = useNavigate();
       const showDetail = () => {
             navigate('/inventory/' + _id);
       };
-    return (
-          <div className='ProductCard animate__animated animate__zoomInUp'>
-                <div>
-                      <img className='My-img-fluid' src={image} alt='' />
-                </div>
-                <div>
-                      <div className=''>
-                            <h1
-                                  className='text-center'
-                                  style={{
-                                        color: '#e03b8b',
-                                  }}
-                            >
-                                  {name}
-                            </h1>
-                            <p className='text-center'>
-                                  {description?.length >= 150
-                                        ? description?.slice(0, 150) + '....'
-                                        : description?.slice(0, 150)}
-                            </p>
-                            <h3 className='text-center'>{price}</h3>
-                            <button
-                                  className='btn btn-Main-color w-100 mt-2'
-                                  onClick={showDetail}
-                            >
-                                  update
-                            </button>
-                            <button
-                                  className='btn btn-success w-100 mt-2'
-                                  onClick={showDetail}
-                            >
-                                  Edit
-                            </button>
-                            <button
-                                  className='btn btn-danger w-100 mt-2'
-                                  onClick={showDetail}
-                            >
-                                  Delete
-                            </button>
-                      </div>
-                </div>
-          </div>
-    );
+
+            const DeleteProduct = (id) => {
+                  const proceed = window.confirm(
+                        'Are you sure you want to delete?'
+                  );
+                  if (proceed) {
+                        const url = `http://localhost:5001/Product/${id}`;
+                        fetch(url, {
+                              method: 'DELETE',
+                        })
+                              .then((res) => res.json())
+                              .then((data) => {
+                                    if (data.deletedCount > 0) {
+                                          const remaining = props.Products.filter(
+                                                (product) => product._id !== id
+                                          );
+                                          props.SetProducts(remaining);
+                                    }
+                              });
+                  }
+            };
+
+      return (
+            <div className='ProductCard animate__animated animate__zoomInUp'>
+                  <div>
+                        <img className='My-img-fluid' src={image} alt='' />
+                  </div>
+                  <div>
+                        <div className=''>
+                              <h1
+                                    className='text-center'
+                                    style={{
+                                          color: '#e03b8b',
+                                    }}
+                              >
+                                    {name}
+                              </h1>
+                              <p className='text-center'>
+                                    {description?.length >= 150
+                                          ? description?.slice(0, 150) + '....'
+                                          : description?.slice(0, 150)}
+                              </p>
+                              <h3 className='text-center'>{price}</h3>
+                              <button
+                                    className='btn btn-Main-color w-100 mt-2'
+                                    onClick={showDetail}
+                              >
+                                    update
+                              </button>
+                              <button
+                                    className='btn btn-success w-100 mt-2'
+                                    onClick={showDetail}
+                              >
+                                    Edit
+                              </button>
+                              <button
+                                    className='btn btn-danger w-100 mt-2'
+                                    onClick={() =>DeleteProduct(_id)}
+                              >
+                                    Delete
+                              </button>
+                        </div>
+                  </div>
+            </div>
+      );
 };
 
 export default ProductCard2;
